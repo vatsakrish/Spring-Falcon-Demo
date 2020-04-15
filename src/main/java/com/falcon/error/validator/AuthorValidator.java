ea09284e9c5a77b73ec7289039a43ad44044e657
Srivatsan Sridhar <59022327+vatsakrish@users.noreply.github.com>
2020-04-15 16:25:51 -0400

    Initial Commit

---
+++ src/main/java/com/falcon/error/validator/AuthorValidator.java
@@ -0,0 +1,18 @@
+package com.falcon.error.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthorValidator implements ConstraintValidator<Author, String> {
+
+    List<String> authors = Arrays.asList("Santideva", "Marie Kondo", "Martin Fowler", "Falcon");
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+
+        return authors.contains(value);
+
+    }
+}
